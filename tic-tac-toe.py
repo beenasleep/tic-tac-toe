7675d90dfd23ba65948cd5699deccdb695591aa1
정준석 <joon123suk@gmail.com>
2017-11-22 22:29:22 +0900

    added game end

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,21 +1,33 @@
 from tkinter import *
 
+def endGame(winner) : # 게임을 종료시키는 함수
+      msgwindow = Tk()
+      msg = Message(msgwindow, text= winner + "의 승리")
+      msg.grid(row=0, column=0)
+      for b in list:
+            b["command"] = ""
 def checked(i) :
       global player
       button = list[i]
 
-      if button["text"] != "     " :
+      if (button["text"] != "     "):
             return
-      button["text"] = player 
+      button["text"] = " "+player+" " 
       button["bg"] = "yellow"
 
-      if player == "X" :
-            player = "O"
-            button["bg"] = "yellow"
+      r = i//3
+      c = i%3
+      
+      if (list[r*3]["text"] == list[r*3 + 1]["text"] == list[r*3 + 2]["text"]) or ( list[c]["text"] == list[c + 3]["text"] == list[c + 6]["text"] ) or ( list[0]["text"] == list[4]["text"] == list[8]["text"] != "     ") or (list[2]["text"] == list[4]["text"] == list[6]["text"] != "     ") :
+            endGame(player)
       else :
-            player = "X"
-            button["bg"] = "lightgreen"
-
+            if player == "X" :
+                  player = "O"
+                  button["bg"] = "yellow"
+            else :
+                  player = "X"
+                  button["bg"] = "lightgreen"
+      
 window = Tk()
 player = "X"
 list= []
@@ -24,7 +36,6 @@ for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
       b.grid(row=i//3, column=i%3)
       list.append(b)
-
 window.mainloop()
 
 
